33ca385e9bc67223cc733ac3f830414d42192c57
Vishruti  Mahajan <140059070+vishrutimahajan@users.noreply.github.com>
2025-06-12 15:54:52 +0530

    Update Feedbacklist.js

--- Feedbacklist.js
+++ Feedbacklist.js
@@ -1,18 +1,28 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Feedbackslist({ feedbacks }) {
+function FeedbacksList({ feedbacks }) {
   return (
     <div>
       <h2>Feedback Submissions</h2>
-      {feedbacks.map((fb, i) => (
-        <div key={i}>
-          <p>{fb.comment}</p>
-       
-        </div>
-      ))}
+      {feedbacks.length === 0 ? (
+        <p>No feedback submitted yet.</p>
+      ) : (
+        feedbacks.map((fb, i) => (
+          <div key={i}>
+            <p><strong>Name:</strong> {fb.name}</p>
+            <p><strong>Course:</strong> {fb.course}</p>
+            <p><strong>Feedback:</strong> {fb.feedback}</p>
+            <hr />
+          </div>
+        ))
+      )}
+      <Link to="/">
+        <button>Back to Home</button>
+      </Link>
     </div>
   );
 }
 
-export default Feedbackslist;
+export default FeedbacksList;
